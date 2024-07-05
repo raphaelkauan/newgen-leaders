bdf2f90cb04c6eba5846cfc637b79b4ca1f85594
raphaelkauan <raphaelkauanoficial@gmail.com>
2024-07-05 17:49:47 -0400

    feat: first commit

---
+++ src/main/java/com/newgenleaders/NewgenLeadersApplication.java
@@ -0,0 +1,13 @@
+package com.newgenleaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgenLeadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewgenLeadersApplication.class, args);
+	}
+
+}
